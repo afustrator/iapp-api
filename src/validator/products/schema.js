ea2899d2da8bdbfa5aeb3cbdcd643cc272fb9148
afustrator <afustrator@gmail.com>
2023-02-27 16:15:04 +0700

    update: edit schema

--- src/validator/products/schema.js
+++ src/validator/products/schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 
 const PostProductPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  stock: Joi.number().integer().required(),
+  stock: Joi.number().integer().min(1).required(),
   price: Joi.number().integer().required(),
   categoryId: Joi.string(),
   expireDate: Joi.number().integer().required()
